7bfef671f6823735dbbaf426b4484d7c93ba89aa
bslota <bartlomiej.slota@gmail.com>
2019-11-12 01:00:39 +0100

    Corner cases tested

--- src/test/java/com/bslota/refactoring/library/BookServiceTest.java
+++ src/test/java/com/bslota/refactoring/library/BookServiceTest.java
@@ -1,7 +1,6 @@
 package com.bslota.refactoring.library;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.stubbing.OngoingStubbing;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
@@ -13,7 +12,9 @@ import static org.mockito.Mockito.when;
 class BookServiceTest {
 
     private static final int NOT_EXISTING_BOOK_ID = 1;
+    private static final int EXISTING_BOOK_ID = 2;
     private static final int NOT_EXISTING_PATRON_ID = 10;
+    private static final int EXISTING_PATRON_ID = 20;
     private static final int HOLDING_PERIOD = 100;
 
     private BookDAO bookDAOStub = mock(BookDAO.class);
@@ -38,12 +39,50 @@ class BookServiceTest {
         assertFalse(bookWasPlacedOnHold);
     }
 
+    @Test
+    void shouldFailToPlaceTheBookOnHoldWhenBookDoesNotExist() {
+        //given:
+        bookThatDoesNotExist();
+
+        //and
+        thereIsAPatron();
+
+        //when:
+        boolean bookWasPlacedOnHold = bookService.placeOnHold(NOT_EXISTING_BOOK_ID, EXISTING_PATRON_ID, HOLDING_PERIOD);
+
+        //then
+        assertFalse(bookWasPlacedOnHold);
+    }
+
+    @Test
+    void shouldFailToPlaceTheBookOnHoldWhenPatronDoesNotExist() {
+        //given:
+        patronThatDoesNotExist();
+
+        //and
+        thereIsABook();
+
+        //when:
+        boolean bookWasPlacedOnHold = bookService.placeOnHold(NOT_EXISTING_BOOK_ID, EXISTING_PATRON_ID, HOLDING_PERIOD);
+
+        //then
+        assertFalse(bookWasPlacedOnHold);
+    }
+
     private void patronThatDoesNotExist() {
         when(patronDAOStub.getPatronFromDatabase(NOT_EXISTING_PATRON_ID)).thenReturn(null);
     }
 
+    private void thereIsAPatron() {
+        when(patronDAOStub.getPatronFromDatabase(EXISTING_PATRON_ID)).thenReturn(new Patron());
+    }
+
     private void bookThatDoesNotExist() {
         when(bookDAOStub.getBookFromDatabase(NOT_EXISTING_BOOK_ID)).thenReturn(null);
+    }
+
+    private void thereIsABook() {
+        when(bookDAOStub.getBookFromDatabase(EXISTING_BOOK_ID)).thenReturn(new Book());
     }
 
 }
