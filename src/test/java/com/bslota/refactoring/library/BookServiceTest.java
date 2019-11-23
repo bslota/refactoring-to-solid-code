82bf56d45bf44f1ecab0acb37780580ceb1fc4b5
bslota <bartlomiej.slota@gmail.com>
2019-11-23 10:31:02 +0100

    missing tests added

--- src/test/java/com/bslota/refactoring/library/BookServiceTest.java
+++ src/test/java/com/bslota/refactoring/library/BookServiceTest.java
@@ -2,12 +2,15 @@ package com.bslota.refactoring.library;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.time.temporal.ChronoUnit.DAYS;
 import static java.util.stream.Collectors.toList;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -21,6 +24,7 @@ class BookServiceTest {
     private static final int PERIOD_IN_DAYS = 100;
     private static final int ID_OF_AVAILABLE_BOOK = 2;
     private static final int ID_OF_PATRON_WITH_MAX_NUMBER_OF_HOLDS = 20;
+    private static final int ID_OF_UNAVAILABLE_BOOK = 3;
 
     private BookDAO bookDAO = mock(BookDAO.class);
     private PatronDAO patronDAO = mock(PatronDAO.class);
@@ -50,6 +54,38 @@ class BookServiceTest {
         assertFalse(result);
     }
 
+    @Test
+    void shouldFailToPlaceBookOnHoldWhenItIsNotAvailable() {
+        //given
+        Patron patron = patronWithMaxNumberOfHolds();
+        Book book = unavailableBook();
+
+        //when:
+        boolean result = bookService.placeOnHold(book.getBookId(), patron.getPatronId(), PERIOD_IN_DAYS);
+
+        //then
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldSucceedToPlaceBookOnHold() {
+        //given
+        Book book = availableBook();
+        Patron patron = patronWithoutHolds();
+
+        //when
+        boolean result = bookService.placeOnHold(book.getBookId(), patron.getPatronId(), PERIOD_IN_DAYS);
+
+        //then
+        assertTrue(result);
+    }
+
+    private Patron patronWithoutHolds() {
+        Patron patron = new Patron(ID_OF_PATRON_WITH_MAX_NUMBER_OF_HOLDS, 0, 0, false, new LinkedList<>());
+        when(patronDAO.getPatronFromDatabase(patron.getPatronId())).thenReturn(patron);
+        return patron;
+    }
+
     private Patron patronWithMaxNumberOfHolds() {
         List<Integer> holds = IntStream.of(1, 2, 3, 4, 5).boxed().collect(toList());
         Patron patron = new Patron(ID_OF_PATRON_WITH_MAX_NUMBER_OF_HOLDS, 0, 0, false, holds);
@@ -59,6 +95,13 @@ class BookServiceTest {
 
     private Book availableBook() {
         Book book = new Book(ID_OF_AVAILABLE_BOOK, null, null, 0);
+        when(bookDAO.getBookFromDatabase(book.getBookId())).thenReturn(book);
+        return book;
+    }
+
+    private Book unavailableBook() {
+        Instant reservationDate = Instant.now();
+        Book book = new Book(ID_OF_UNAVAILABLE_BOOK, reservationDate, reservationDate.plus(PERIOD_IN_DAYS, DAYS), 0);
         when(bookDAO.getBookFromDatabase(book.getBookId())).thenReturn(book);
         return book;
     }
